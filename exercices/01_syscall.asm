0515fc45042550bd405649252522403a14e1c5e9
Herael <b.rousval@hotmail.com>
2020-03-25 10:36:53 +0100

    do exercices

--- exercices/01_syscall.asm
+++ exercices/01_syscall.asm
@@ -1,11 +1,7 @@
 # Exercice 1
 # utiliser un appel système comme write
-
-# On indique qu'on souhaite ranger des données dans le segment de data.
 .data
-
-# la directive .asciz permet de déclarer une chaine fini par un 0x00, '\0' en ascii.
-msg: .asciz "Hello ASM from .data"
+msg: .asciz "Hello World form .data"
 
 .text # <- segment de code.
 
@@ -14,8 +10,8 @@ msg: .asciz "Hello ASM from .data"
 # - Dans quelle segment de la mémoire est elle?
 # - Quelle sont les valeurs en hexadecimales présente dans .data?
 
-la a0, ??? # argument de l'appel systeme
-li a7, ??? # appel systeme 4 selon la doc de Rars printString
-ecall # Peret de basculer en mode kernel pour faire l'appel systeme.
+la a0, msg
+li a7, 4 # appel systeme 4 selon la doc de Rars printString
+ecall
 
-# Bonus: Afficher maintenant un nombre, vous aurrez besoin de la documentation.
+# Bonus: avoir 64 dans un registre en une seule instruction
